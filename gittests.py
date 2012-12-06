7930e9c1472adae0bc3d81f73903adb019735d1b
Sascha Peilicke <saschpe@suse.de>
2012-12-06 11:11:40 +0100

    Add test for @PARENT_TAG@

--- gittests.py
+++ gittests.py
@@ -15,3 +15,7 @@ class GitTests(GitHgTests):
 
     def default_version(self):
         return self.timestamps(self.rev(2))
+
+    def test_versionformat_parenttag(self):
+        self.tar_scm_std('--versionformat', "@PARENT_TAG@")
+        self.assertTarOnly(self.basename(version = self.rev(2)))
