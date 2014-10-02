4c1ff9201d0d8931d5a84e361bfe0812bf52a963
Adam Spiers <opensuse@adamspiers.org>
2014-10-02 16:37:38 +0100

    add tests for changesgenerate

--- tests/gittests.py
+++ tests/gittests.py
@@ -2,7 +2,9 @@
 
 import datetime
 import os
+import re
 import tarfile
+import textwrap
 
 from   githgtests  import GitHgTests
 from   gitfixtures import GitFixtures
@@ -109,3 +111,133 @@ class GitTests(GitHgTests):
                                        self.basename(version = 'tag3')+'.tar'))
         self.assertRaises(KeyError, th.getmember, os.path.join(
             self.basename(version = 'tag3'), submod_name, 'a'))
+
+    def _check_servicedata(self, expected_dirents=2, revision=2):
+        expected_sha1 = self.sha1s('tag%d' % revision)
+        dirents = self.assertNumDirents(self.outdir, expected_dirents)
+        self.assertIn('_servicedata', dirents)
+        sd = open(os.path.join(self.outdir, '_servicedata')).read()
+        expected = """\
+          <servicedata>
+            <service name="tar_scm">
+              <param name="url">%s</param>
+              <param name="changesrevision">([0-9a-f]{40})</param>
+            </service>
+          </servicedata>""" % self.fixtures.repo_url
+        (expected, count) = re.subn('\s{2,}', '\s*', expected)
+        m = re.match(expected, sd)
+        self.assertTrue(m, "\n'%s'\n!~ /%s/" % (sd, expected))
+        sha1 = m.group(1)
+        self.assertEqual(sha1, expected_sha1)
+
+    def test_changesgenerate_disabled(self):
+        self.tar_scm_std('--changesgenerate', 'disable')
+
+    def test_changesgenerate_no_servicedata(self):
+        self.tar_scm_std('--changesgenerate', 'enable')
+        self._check_servicedata()
+
+    def test_changesgenerate_corrupt_servicedata(self):
+        with open(os.path.join(self.pkgdir, '_servicedata'), 'w') as sd:
+            sd.write('this is not valid xml')
+        self.tar_scm_std('--changesgenerate', 'enable', should_succeed=False)
+
+    def test_changesgenerate_empty_servicedata_file(self):
+        sd = open(os.path.join(self.pkgdir, '_servicedata'), 'w')
+        sd.close()
+        self.tar_scm_std('--changesgenerate', 'enable')
+        self._check_servicedata()
+
+    def test_changesgenerate_empty_servicedata_element(self):
+        with open(os.path.join(self.pkgdir, '_servicedata'), 'w') as sd:
+            sd.write("<servicedata>\n</servicedata>\n")
+        self.tar_scm_std('--changesgenerate', 'enable')
+        self._check_servicedata()
+
+    def test_changesgenerate_no_changesrevision(self):
+        with open(os.path.join(self.pkgdir, '_servicedata'), 'w') as sd:
+            sd.write(textwrap.dedent("""\
+              <servicedata>
+                <service name="tar_scm">
+                  <param name="url">%s</param>
+                </service>
+              </servicedata>
+            """ % self.fixtures.repo_url))
+        self.tar_scm_std('--changesgenerate', 'enable')
+        self._check_servicedata()
+
+    def _write_servicedata(self, rev):
+        sha1 = self.sha1s('tag%d' % rev)
+        with open(os.path.join(self.pkgdir, '_servicedata'), 'w') as sd:
+            sd.write(textwrap.dedent("""\
+              <servicedata>
+                <service name="tar_scm">
+                  <param name="url">%s</param>
+                  <param name="changesrevision">%s</param>
+                </service>
+              </servicedata>
+            """ % (self.fixtures.repo_url, sha1)))
+
+    def _write_changes_file(self):
+        contents = textwrap.dedent("""\
+          -------------------------------------------------------------------
+          Fri Oct  3 00:17:50 BST 2014 - %s
+
+          - 2
+
+          -------------------------------------------------------------------
+          Thu Sep 18 10:27:14 BST 2014 - %s
+
+          - 1
+        """ % (self.fixtures.user_email, self.fixtures.user_email))
+        with open(os.path.join(self.pkgdir, 'pkg.changes'), 'w') as f:
+            f.write(contents)
+        return contents
+
+    def test_changesgenerate_no_change_or_changes_file(self):
+        self._write_servicedata(2)
+        self.tar_scm_std('--changesgenerate', 'enable')
+        self._check_servicedata()
+
+    def test_changesgenerate_no_change_same_changes_file(self):
+        self._write_servicedata(2)
+        orig_changes = self._write_changes_file()
+        self.tar_scm_std('--changesgenerate', 'enable')
+        self._check_servicedata()
+
+    def test_changesgenerate_new_commit_no_changes_file(self):
+        self._write_servicedata(2)
+        self.fixtures.create_commits(1)
+        self.tar_scm_std('--changesgenerate', 'enable')
+        self._check_servicedata(revision=3)
+
+    def test_changesgenerate_new_commit_and_changes_file(self):
+        self._write_servicedata(2)
+        orig_changes = self._write_changes_file()
+        self.fixtures.create_commits(3)
+        self.tar_scm_std(
+            '--changesgenerate', 'enable',
+            '--versionformat', '0.6.%h',
+            '--changesauthor', self.fixtures.user_email,
+        )
+        self._check_servicedata(revision=5, expected_dirents=3)
+
+        new_changes_file = os.path.join(self.outdir, 'pkg.changes')
+        expected_changes_regexp = textwrap.dedent("""\
+          ^-------------------------------------------------------------------
+          \w{3} \w{3} [ \d]\d \d\d:\d\d:\d\d [A-Z]{3} 20\d\d - %s
+
+          - Update to version 0.6.%s:
+            \+ 3
+            \+ 4
+            \+ 5
+
+          (.+)""" % (self.fixtures.user_email, self.abbrev_sha1s('tag5')))
+        self.assertTrue(os.path.exists(new_changes_file))
+        with open(new_changes_file) as f:
+            new_changes = f.read()
+            self.assertNotEqual(orig_changes, new_changes)
+            print new_changes
+            self.assertRegexpMatches(new_changes, expected_changes_regexp)
+            m = re.match(expected_changes_regexp, new_changes, re.DOTALL)
+            self.assertEqual(m.group(1), orig_changes)
