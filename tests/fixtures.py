4470dc623fc82c9de91eef647ac11eb59adebfb0
David Disseldorp <ddiss@suse.de>
2017-01-31 15:31:01 +0100

    tests: add helper for GPG key-pair setup
    The GPG key-pair is created in a "gpg" subdirectory under the parent
    test dir. Once initialised, the GNUPGHOME environment variable is set
    for all safe_run() command invocations, ensuring that it can be used
    for signing and verification.
    
    Both Git and Bazaar support signing and verification, so add this
    functionality to the common Fixtures class.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -2,6 +2,8 @@
 
 import os
 import shutil
+import re
+from utils import run_cmd
 
 
 class Fixtures:
@@ -27,12 +29,17 @@ class Fixtures:
         self.scmlogs       = scmlogs
         self.repo_path     = self.container_dir + '/repo'
         self.repo_url      = 'file://' + self.repo_path
+        self.gpg_dir       = None
+        self.gpg_key_id    = None
 
         # Keys are stringified integers representing commit sequence numbers;
         # values can be passed to --revision
         self.revs = {}
 
     def safe_run(self, cmd):
+        if self.gpg_dir != None:
+            os.putenv('GNUPGHOME', self.gpg_dir)
+
         stdout, stderr, exitcode = self.run(cmd)
         if exitcode != 0:
             raise RuntimeError("Command failed; aborting.")
@@ -137,3 +144,64 @@ class Fixtures:
         self.do_commit(wd, new_rev, newly_created)
         self.record_rev(wd, new_rev)
         self.scmlogs.annotate("Created 1 commit; now at %s" % (new_rev))
+
+    def create_gpg_key(self):
+
+        if self.gpg_dir:
+            print 'GPG state already initialised'
+            return (self.gpg_dir, self.gpg_key_id)
+
+        batch_script = '''
+            %echo starting keygen
+            Key-Type: default
+            Subkey-Type: default
+            Name-Real: ''' + self.user_name + '''
+            Name-Comment: test user
+            Name-Email: ''' + self.user_email + '''
+            Expire-Date: 1d
+            %no-protection
+            %transient-key
+            %commit
+            %echo done'''
+
+        # create a dir to use as GNUPGHOME for key import and verification
+        gpg_dir = self.container_dir + "/gpg"
+        os.makedirs(gpg_dir)
+        batch_path = gpg_dir + "/batch_script.txt"
+
+        with open(batch_path, "w") as text_file:
+            text_file.write(batch_script)
+
+        cmd = 'gpg --homedir %s --gen-key --batch %s' % (gpg_dir, batch_path)
+        (stdout, stderr, ret) = run_cmd(cmd)
+        if ret != 0:
+            print "GPG key creation failed: ", stderr
+            return (None, None)
+
+        cmd = 'gpg --homedir %s --keyid-format LONG --list-secret-keys' \
+                % (gpg_dir)
+
+        (stdout, stderr, ret) = run_cmd(cmd)
+        if ret != 0:
+            print "GPG key list failed: ", stderr
+            return (None, None)
+
+        m = re.search('sec\s+\w+/(\w+)', stdout)
+        if m == None or m.group(1) == None:
+            print "couldn't find GPG key ID in ", stdout
+            return (None, None)
+
+        gpg_key_id = m.group(1)
+
+        print "created GPG keypair at %s with key id %s" % (gpg_dir, gpg_key_id)
+        self.gpg_dir = gpg_dir
+        self.gpg_key_id = gpg_key_id
+        return (gpg_dir, gpg_key_id)
+
+    def delete_gpg_key(self):
+        if self.gpg_dir == None:
+            return
+
+        shutil.rmtree(self.gpg_dir)
+        self.gpg_dir = None
+        self.gpg_key_id = None
