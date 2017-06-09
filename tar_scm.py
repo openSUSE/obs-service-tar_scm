14ac1daeecfd94d140b57afb86b807dd23e936ca
Frank Schreiner <schreiner@suse.de>
2017-06-09 16:27:25 +0200

    [lint] cleanup for tar_scm.py
    ************* Module tar_scm
    C: 20, 0: Import "import TarSCM" should be placed at the top of the module (wrong-import-position)
    C: 21, 0: Import "import TarSCM.tasks" should be placed at the top of the module (wrong-import-position)
    C: 22, 0: Import "from TarSCM.exceptions import OptionsError" should be placed at the top of the module (wrong-import-position)
    W: 22, 0: Unused OptionsError imported from TarSCM.exceptions (unused-import)
    
    -----------------------------------
    Your code has been rated at 6.00/10

--- tar_scm.py
+++ tar_scm.py
@@ -17,9 +17,10 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))  # noqa
 
-import TarSCM
-import TarSCM.tasks
-from TarSCM.exceptions import OptionsError
+# These two modules have to be imported after sys.path.append because
+# the additional path is where the modules are found
+import TarSCM        # pylint: disable=wrong-import-position
+import TarSCM.tasks  # pylint: disable=wrong-import-position
 
 
 def main():
