a3a20f5b62c1b78e925a989f8cd912bbef129a14
doccaz <erico.mendonca@suse.com>
2018-03-06 16:35:16 -0300

    fixing indentation warnings from flake8

--- TarSCM/tasks.py
+++ TarSCM/tasks.py
@@ -50,9 +50,8 @@ class Tasks():
         # gets raised
         if self.scm_object:
             self.scm_object.unlock_cache()
-	    # calls the corresponding cleanup routine
-	    self.scm_object.cleanup()
-
+            # calls the corresponding cleanup routine
+            self.scm_object.cleanup()
 
     def generate_list(self):
         '''
