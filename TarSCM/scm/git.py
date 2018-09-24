openSUSE/obs-service-tar_scm

commit 44b3beeaf8663d46467fb3945ddc3d73c65ac452

check url for remote url

TarSCM/scm/git.py
@@ -321,3 +321,9 @@ class Git(Scm):
     # no cleanup is necessary for git
     def cleanup(self):
         pass
+
+    def check_url(self):
+        """check if url is a remote url"""
+        if not re.match("^(https?|ftps?|git)://", self.url):
+            return False
+        return True

tests/unittestcases.py
@@ -15,6 +15,8 @@ from TarSCM.config  import Config
 from TarSCM.changes import Changes
 from TarSCM.scm.git import Git
 from TarSCM.scm.svn import Svn
+from TarSCM.scm.hg  import Hg
+from TarSCM.scm.bzr import Bzr
 
 if sys.version_info < (2, 7):
     # pylint: disable=import-error
@@ -260,3 +262,75 @@ class UnitTestCases(unittest.TestCase):
             re.compile('ERROR: no .obsinfo file found in directory:'),
             scm_object.fetch_upstream
         )
+
+    def test_check_url_valid(self):
+        tc_arr = [
+            {
+                'obj'  : Git(self.cli, self.tasks),
+                'urls' : [
+                    'http://example.com',
+                    'https://example.com',
+                    'ftp://example.com',
+                    'ftps://example.com',
+                    'git://example.com',
+                ]
+            },
+            {
+                'obj'  : Bzr(self.cli, self.tasks),
+                'urls' : [
+                    'http://example.com',
+                    'https://example.com',
+                    'ftp://example.com',
+                    'aftp://example.com',
+                    'bzr://example.com',
+                    'lp://example.com',
+                ]
+            },
+            {
+                'obj'  : Hg(self.cli, self.tasks),
+                'urls' : [
+                    'http://example.com',
+                    'https://example.com',
+                ]
+            },
+            {
+                'obj'  : Svn(self.cli, self.tasks),
+                'urls' : [
+                    'http://example.com',
+                    'https://example.com',
+                    'svn://example.com',
+                ]
+            },
+        ]
+
+        for tca in tc_arr:
+            for url in tca['urls']:
+                tca['obj'].url = url
+                self.assertTrue(tca['obj'].check_url())
+
+    def test_check_url_invalid(self):
+        invalid = [
+            'Xhttp://example.com',
+            'Xhttps://example.com',
+            'Xftp://example.com',
+            'Xftps://example.com',
+            'Xaftp://example.com',
+            'Xbzr://example.com',
+            'Xlp://example.com',
+            'Xgit://example.com',
+            'Xsvn://example.com',
+            '/lala/nana'
+        ]
+
+        scms = [
+            Git(self.cli, self.tasks),
+            Bzr(self.cli, self.tasks),
+            Hg(self.cli, self.tasks),
+            Svn(self.cli, self.tasks),
+        ]
+
+        for scm in scms:
+            for url in invalid:
+                print("%r %s" % (scm, url))
+                scm.url = url
+                self.assertFalse(scm.check_url())

GNUmakefile
@@ -99,11 +99,11 @@ flake8:
 .PHONY: test
 test:
 	: Running the test suite.  Please be patient - this takes a few minutes ...
-	PYTHONPATH=. $(PYTHON) tests/test.py 2>&1 | tee ./test.log
+	TAR_SCM_TESTMODE=1 PYTHONPATH=. $(PYTHON) tests/test.py 2>&1 | tee ./test.log
 
 test3:
 	: Running the test suite.  Please be patient - this takes a few minutes ...
-	PYTHONPATH=. python3 tests/test.py 2>&1 | tee ./test3.log
+	TAR_SCM_TESTMODE=1 PYTHONPATH=. python3 tests/test.py 2>&1 | tee ./test3.log
 
 .PHONY: pylint
 pylint: pylint2

TarSCM/scm/base.py
@@ -290,3 +290,6 @@ class Scm():
 
     def finalize(self):
         self.cleanup()
+
+    def check_url(self):
+        return True

TarSCM/scm/bzr.py
@@ -65,3 +65,9 @@ class Bzr(Scm):
     # no cleanup is necessary for bzr
     def cleanup(self):
         pass
+
+    def check_url(self):
+        """check if url is a remote url"""
+        if not re.match("^((a?ftp|bzr|https?)://|lp:)", self.url):
+            return False
+        return True

TarSCM/scm/hg.py
@@ -138,3 +138,9 @@ class Hg(Scm):
         except:
                 logging.debug("error on cleanup:", sys.exc_info()[0])
                 raise
+
+    def check_url(self):
+        """check if url is a remote url"""
+        if not re.match("^https?://", self.url):
+            return False
+        return True

TarSCM/scm/svn.py
@@ -195,3 +195,9 @@ class Svn(Scm):
         except:
                 logging.debug("error on cleanup:", sys.exc_info()[0])
                 raise
+
+    def check_url(self):
+        """check if url is a remote url"""
+        if not re.match("^(https?|svn)://", self.url):
+            return False
+        return True

TarSCM/tasks.py
@@ -171,6 +171,11 @@ class Tasks():
         # if exception occurs
         self.scm_object = scm_object   = scm_class(args, self)
 
+        # TODO: find a way to mock this function in tests to a stub
+        if not bool(os.getenv('TAR_SCM_TESTMODE')):
+            if not scm_object.check_url():
+                sys.exit("--url does not match remote repository")
+
         try:
             scm_object.check_scm()
         except OSError:

commit 30c78b0401772b6cf76feaf2aeeed744948ba2bf

Support also SSH urls for Git
ssh:// URLs are common in private OBS deployments.

Regression got introduced in:
44b3beeaf8663d46467fb3945ddc3d73c65ac452

TarSCM/scm/git.py
@@ -324,6 +324,6 @@ class Git(Scm):
 
     def check_url(self):
         """check if url is a remote url"""
-        if not re.match("^(https?|ftps?|git)://", self.url):
+        if not re.match("^(https?|ftps?|git|ssh)://", self.url):
             return False
         return True

tests/unittestcases.py
@@ -273,6 +273,7 @@ class UnitTestCases(unittest.TestCase):
                     'ftp://example.com',
                     'ftps://example.com',
                     'git://example.com',
+                    'ssh://example.com',
                 ]
             },
             {
@@ -318,6 +319,7 @@ class UnitTestCases(unittest.TestCase):
             'Xbzr://example.com',
             'Xlp://example.com',
             'Xgit://example.com',
+            'Xssh://example.com',
             'Xsvn://example.com',
             '/lala/nana'
         ]