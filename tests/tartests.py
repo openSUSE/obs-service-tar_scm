e8a6f25371f7981c91e83b4b3cab60a495d6ae16
Egbert Eich <eich@suse.com>
2019-08-04 14:46:44 +0200

    Fix unit tests for modified scm directory handling
    Signed-off-by: Egbert Eich <eich@suse.com>

--- tests/tartests.py
+++ tests/tartests.py
@@ -29,12 +29,13 @@ class TarTestCases(TestEnvironment, TestAssertions):
         obsinfo = open(info, 'w')
         obsinfo.write(
             "name: pkgname\n" +
+            "scmdir: pkgname_service\n" +
             "version: 0.1.1\n" +
             "mtime: 1476683264\n" +
             "commit: fea6eb5f43841d57424843c591b6c8791367a9e5\n"
         )
         obsinfo.close()
-        src_dir = os.path.join(wdir, "pkgname")
+        src_dir = os.path.join(wdir, "pkgname_service")
         os.mkdir(src_dir)
         self.tar_scm_std()
         self.assertTrue(os.path.isdir(src_dir))
