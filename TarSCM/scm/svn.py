openSUSE/obs-service-tar_scm

commit 805922e7b2f7bd2ce34f042681714ed4d3936aa6

Use cli params to set credentials for Subversion SCM

TarSCM/scm/svn.py
@@ -76,6 +76,10 @@ class Svn(Scm):
             cfg.close()
             scmcmd += ['--config-dir', self.svntmpdir]
 
+            if self.user and self.password:
+                scmcmd += ['--username', self.user]
+                scmcmd += ['--password', self.password]
+
         return scmcmd
 
     def fetch_upstream_scm(self):

commit 6521c1001b4bfa6af8de54999881ea978ddbe72e

Fix setting svn credentials in the command line
Fixes: 805922e7 ("Use cli params to set credentials for Subversion SCM")

TarSCM/scm/svn.py
@@ -76,9 +76,9 @@ class Svn(Scm):
             cfg.close()
             scmcmd += ['--config-dir', self.svntmpdir]
 
-            if self.user and self.password:
-                scmcmd += ['--username', self.user]
-                scmcmd += ['--password', self.password]
+        if self.user and self.password:
+            scmcmd += ['--username', self.user]
+            scmcmd += ['--password', self.password]
 
         return scmcmd
 