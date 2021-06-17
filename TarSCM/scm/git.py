openSUSE/obs-service-tar_scm

commit 49bb443c6011f0a4da0469e37cbbe7df1f0e4b97

don`t sanitize version if versionrewrite_pattern is set
If versionrewrite_pattern is used we assume you know what you are doing.
If it is not set we sanitize the version at the end avoid corrupt spec
e.g if version hyphens.

TarSCM/scm/git.py
@@ -237,7 +237,6 @@ class Git(Scm):
         """
         self._parent_tag = args['parent_tag'] or self._parent_tag
         versionformat = args['versionformat']
-        debian = args.get('use_obs_gbp', False)
         if versionformat is None:
             versionformat = '%ct.%h'
 
@@ -259,7 +258,7 @@ class Git(Scm):
                                    "--pretty=format:%s" % versionformat],
             self.clone_dir
         )[1]
-        return self.version_iso_cleanup(version, debian)
+        return version
 
     def _detect_parent_tag(self, args=None):
         parent_tag = ''

TarSCM/scm/hg.py
@@ -125,8 +125,7 @@ class Hg(Scm):
             versionformat
         ])
 
-        version = self.helpers.safe_run(cmd, self.clone_dir)[1]
-        return self.version_iso_cleanup(version)
+        return self.helpers.safe_run(cmd, self.clone_dir)[1]
 
     def get_timestamp(self):
         data = {"parent_tag": None, "versionformat": "{date}"}

TarSCM/scm/svn.py
@@ -98,7 +98,7 @@ class Svn(Scm):
                                   interactive=sys.stdout.isatty())
         except SystemExit as exc:
             if re.search(ENCODING_RE, exc.code):
-                raise SystemExit(ENCODING_MSG)
+                raise SystemExit(ENCODING_MSG)  # pylint: disable=W0707
             raise exc
 
     def update_cache(self):
@@ -118,7 +118,7 @@ class Svn(Scm):
                 shutil.rmtree(self.clone_dir)
                 self.fetch_upstream_scm()
             elif re.search(ENCODING_RE, exc.code):
-                raise SystemExit(ENCODING_MSG)
+                raise SystemExit(ENCODING_MSG)  # pylint: disable=W0707
             else:
                 raise exc
 

TarSCM/tasks.py
@@ -165,7 +165,8 @@ class Tasks():
         try:
             scm_class = getattr(TarSCM.scm, scm2class[args.scm])
         except:
-            raise OptionsError("Please specify valid --scm=... options")
+            msg = "Please specify valid --scm=... options"
+            raise OptionsError(msg)  # pylint: disable=W0707
 
         # self.scm_object is need to unlock cache in cleanup
         # if exception occurs
@@ -269,6 +270,10 @@ class Tasks():
         if self.args.versionrewrite_pattern:
             regex = re.compile(self.args.versionrewrite_pattern)
             version = regex.sub(self.args.versionrewrite_replacement, version)
+        else:
+            args = self.args.__dict__
+            debian = args.get('use_obs_gbp', False)
+            version = self.scm_object.version_iso_cleanup(version, debian)
         if self.args.versionprefix:
             version = "%s.%s" % (self.args.versionprefix, version)
 

tests/fake_classes.py
@@ -23,3 +23,6 @@ class FakeSCM():
     # pylint: disable=too-few-public-methods
     def detect_version(self, args):
         return self.version
+
+    def version_iso_cleanup(self, version, debian):
+        return self.version

commit c4a80d52de35a9aa08237d72a0fba961a07037d3

Change date format from short to %Y%m%d.
Since commit 49bb443 ("don`t sanitize version if versionrewrite_pattern
is set") the dashes from the short date format get into the version
string when versionrewrite_pattern is specified.

Change the date format so that no dashes are included in the date to
start with so that we don't need to rely on the sanitization to remove
them again.

Fixes: #419
Fixes: 49bb443 ("don`t sanitize version if versionrewrite_pattern is set")
Signed-off-by: Michal Suchanek <msuchanek@suse.de>

TarSCM/scm/git.py
@@ -252,7 +252,7 @@ class Git(Scm):
             versionformat = self._detect_version_tag_offset(
                 self._parent_tag,
                 versionformat)
-        log_cmd = self._get_scm_cmd() + ['log', '-n1', '--date=short',
+        log_cmd = self._get_scm_cmd() + ['log', '-n1', '--date=format:%Y%m%d',
                                          "--pretty=format:%s" % versionformat]
         if self.revision:
             log_cmd.append('--source')