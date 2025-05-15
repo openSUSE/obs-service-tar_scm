openSUSE/obs-service-tar_scm

commit 140a127a1073d474733e801d1880bf4675bae7c3

fix excludes for obscpio

TarSCM/archive.py
@@ -17,7 +17,7 @@ try:
 except:
     from StringIO import StringIO
 
-METADATA_PATTERN = re.compile(r'.*/\.(bzr|git|hg|svn)(\/.*|$)')
+METADATA_PATTERN = re.compile(r'.*/\.(bzr|git|hg|svn)(/.*|$)')
 
 
 class BaseArchive():
@@ -85,14 +85,14 @@ class ObsCpio(BaseArchive):
         # transform glob patterns to regular expressions
         includes  = ''
         excludes  = r'$.'
-        topdir_re = '(' + re.escape(topdir) + '/)('
+        re_topdir = '(%s)/(%s)'
+
         if args.include:
             incl_arr = [fnmatch.translate(x + '*') for x in args.include]
-            match_list = r'|'.join(incl_arr)
-            includes = topdir_re + match_list + ')'
+            includes = re_topdir % (re.escape(topdir), r'|'.join(incl_arr))
         if args.exclude:
-            excl_arr = [fnmatch.translate(x) for x in args.exclude]
-            excludes = topdir_re + r'|'.join(excl_arr) + ')'
+            excl_arr = [x for x in args.exclude]
+            excludes = re_topdir % (re.escape(topdir), r'|'.join(excl_arr))
 
         # add topdir without filtering for now
         cpiolist = []

commit ab7ef8805a49dc24cc708f1cd12b00806e032de5

Use fnmatch again for excludes
According to the documentation, the value of the "excludes" parameter for
obs_scm is a glob. But since commit 140a127 ("fix excludes for obscpio") the
code treats this value as a regular expression, which leads to wrong matching
results and may lead to python crashes if the glob expression starts with "*".

Fixes: https://github.com/openSUSE/obs-service-tar_scm/issues/520
Fixes: 140a127 ("fix excludes for obscpio")

TarSCM/archive.py
@@ -61,7 +61,7 @@ class BaseArchive():
             incl_arr = [fnmatch.translate(x + '*') for x in args.include]
             includes = re_topdir % (re.escape(topdir), r'|'.join(incl_arr))
         if args.exclude:
-            excl_arr = [x for x in args.exclude]
+            excl_arr = [fnmatch.translate(x) for x in args.exclude]
             excludes = re_topdir % (re.escape(topdir), r'|'.join(excl_arr))
 
         # add topdir without filtering for now